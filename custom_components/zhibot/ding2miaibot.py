1dfad8716535b9b25f3b0f99a54fc15132cb097c
Yonsm <Yonsm@qq.com>
2021-01-02 00:58:14 +0800

    refine

--- custom_components/zhibot/ding2miaibot.py
+++ custom_components/zhibot/ding2miaibot.py
@@ -14,14 +14,12 @@ class ding2miaibotView(dingbotView):
     async def handleQuery(self, query):
         if query.startswith('音量'):
             pos = query.find('%')
-            if pos == -1:
-                volume = query[2:]
-                message = None
-            else:
-                volume = query[2:pos]
-                message = query[pos+1:]
-            await async_send_message('miai', message, {'volume': volume})
-            return '已设置音量：' + volume + (('，并喊话：' + message) if message else '')
-        await async_send_message('miai', query)
-        return '已喊话：' + query
- 
+            message = None if pos == -1 else query[pos+1:]
+            data = {'volume': query[2:None if if pos == -1 else pos]}
+            answer = '已设置音量：' + volume + (('，并喊话：' + message) if message else '') 
+        else:
+            message = query
+            data = {}
+            answer = '已喊话：' + query
+        await async_send_message('miai', query, data)
+        return answer
